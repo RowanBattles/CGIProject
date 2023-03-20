3ecdecaf0f68e0a90ecd1d44359d39e325a60ec2
Kubilay Karabulut <PS215264@summacollege.nl>
2023-03-20 14:50:54 +0100

    CRUD for Journey has been added (NO CSS)

--- CGI/Controllers/JourneyController.cs
+++ CGI/Controllers/JourneyController.cs
@@ -1,9 +1,7 @@
 ﻿﻿using CGI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using System.Data.SqlTypes;
 
-using System.Text.Json;
 
 namespace CGI.Controllers
 {
@@ -16,338 +14,178 @@ namespace CGI.Controllers
             _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        public IActionResult Index()
+        // Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Journey journey)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Journeys (User_ID, Total_Distance, Total_Emission, Start, End, Date) VALUES (@User_ID, @Total_Distance, @Total_Emission, @Start, @End, @Date)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@User_ID", journey.User_ID);
+                    cmd.Parameters.AddWithValue("@Total_Distance", journey.Total_Distance);
+                    cmd.Parameters.AddWithValue("@Total_Emission", journey.Total_Emission);
+                    cmd.Parameters.AddWithValue("@Start", journey.Start);
+                    cmd.Parameters.AddWithValue("@End", journey.End);
+                    cmd.Parameters.AddWithValue("@Date", journey.Date);
+
+                    conn.Open();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // Read
+        public async Task<IActionResult> Index()
         {
             List<Journey> journeys = new List<Journey>();
 
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
-                connection.Open();
-
-                string sql = @"SELECT j.Journey_ID, j.User_ID, j.Start, j.[End], j.Date, s.Stopover_ID, s.Vehicle_ID, s.Distance, s.Start, s.[End], s.Emission, SUM(s.Distance) OVER (PARTITION BY j.Journey_ID) AS Total_Distance, SUM(s.Emission) OVER (PARTITION BY j.Journey_ID) AS Total_Emission FROM Journeys j JOIN Stopovers s ON j.Journey_ID = s.Journey_ID ORDER BY j.Date DESC";
-
-
-                SqlCommand command = new SqlCommand(sql, connection);
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Journeys", conn))
                 {
-                    int journeyID = (int)reader["Journey_ID"];
-                    Journey journey = journeys.FirstOrDefault(j => j.JourneyID == journeyID);
-
-                    if (journey == null)
+                    conn.Open();
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
                     {
-                        journey = new Journey
+                        while (await reader.ReadAsync())
                         {
-                            JourneyID = journeyID,
-                            UserID = (int)reader["User_ID"],
-                            TotalDistance = (int)reader["Total_Distance"],
-                            TotalEmission = (int)reader["Total_Emission"],
-                            Start = (string)reader["Start"],
-                            End = (string)reader["End"],
-                            Date = ((DateTime)reader["Date"]).Date,
-                            Stopovers = new List<Stopover>()
-                        };
-                        journeys.Add(journey);
+                            Journey journey = new Journey
+                            {
+                                Journey_ID = reader.GetInt32(0),
+                                User_ID = reader.GetInt32(1),
+                                Total_Distance = reader.GetInt32(2),
+                                Total_Emission = reader.GetInt32(3),
+                                Start = reader.GetString(4),
+                                End = reader.GetString(5),
+                                Date = reader.GetDateTime(6)
+                            };
+                            journeys.Add(journey);
+                        }
                     }
-
-                    Stopover stopover = new Stopover
-                    {
-                        StopoverID = (int)reader["Stopover_ID"],
-                        JourneyID = journeyID,
-                        VehicleID = (int)reader["Vehicle_ID"],
-                        Distance = (int)reader["Distance"],
-                        Start = (string)reader["Start"],
-                        End = (string)reader["End"],
-                        Emission = (int)reader["Emission"],
-                    };
-
-                    journey.Stopovers.Add(stopover);
                 }
-
-                reader.Close();
             }
 
             return View(journeys);
         }
 
-
-        public IActionResult Details(int id)
+        // Update
+        public async Task<IActionResult> Edit(int id)
         {
-            // Fetch a specific journey from the database
-            Journey journey = GetJourneyById(id);
+            Journey journey;
 
-            if (journey == null)
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
-                return NotFound();
-            }
-
-            journey.Stopovers = GetStopoversForJourney(id);
-            return View(journey);
-        }
-
-        private Journey GetJourneyById(int id)
-        {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                string sql = "SELECT * FROM Journeys WHERE Journey_ID = @JourneyID";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Journeys WHERE Journey_ID = @Journey_ID", conn))
                 {
-                    command.Parameters.AddWithValue("@JourneyID", id);
+                    cmd.Parameters.AddWithValue("@Journey_ID", id);
 
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.Read())
+                    conn.Open();
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
                     {
-                        Journey journey = new Journey
+                        if (!await reader.ReadAsync())
                         {
-                            JourneyID = (int)reader["Journey_ID"],
-                            UserID = (int)reader["User_ID"],
-                            TotalDistance = (int)reader["Total_Distance"],
-                            TotalEmission = (int)reader["Total_Emission"],
-                            Start = (string)reader["Start"],
-                            End = (string)reader["End"],
-                            Date = ((DateTime)reader["Date"]).Date
-                        };
+                            return NotFound();
+                        }
 
-                        reader.Close();
-
-                        return journey;
-                    }
-                }
-            }
-            return null;
-        }
-
-        private List<Stopover> GetStopoversForJourney(int journeyId)
-        {
-            List<Stopover> stopovers = new List<Stopover>();
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                string sql = "SELECT * FROM Stopovers WHERE Journey_ID = @JourneyID";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@JourneyID", journeyId);
-
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        Stopover stopover = new Stopover
-                        {
-                            StopoverID = (int)reader["Stopover_ID"],
-                            JourneyID = (int)reader["Journey_ID"],
-                            VehicleID = (int)reader["Vehichle_ID"],
-                            Distance = (int)reader["Distance"],
-                            Start = (string)reader["Start"],
-                            End = (string)reader["End"],
-                            Emission = (int)reader["Emission"],
-
-                        };
-
-                        stopovers.Add(stopover);
-                    }
-                    reader.Close();
-                }
-            }
-            return stopovers;
-        }
-
-        public IActionResult Create()
-        {
-            Stopover model = new Stopover();
-            return View(model);
-        }
-
-        public IActionResult CreateCookie(Stopover model)
-        {
-            var cookieOptions = new CookieOptions();
-            cookieOptions.Expires = DateTime.Now.AddDays(2);
-            cookieOptions.Path = "/"; 
-
-            string test = JsonSerializer.Serialize(model);
-
-            Response.Cookies.Append("Stopover", test, cookieOptions);
-            return RedirectToAction(nameof(Create));
-        }
-
-        [HttpPost]
-        public IActionResult Create(Journey journey)
-        {         
-            // Insert a new journey into the database
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                string sql = "INSERT INTO Journeys (User_ID, Total_Distance, Total_Emission, Start, [End], Date) VALUES (@UserID, @TotalDistance, @TotalEmission, @Start, @End, @Date); SELECT SCOPE_IDENTITY();";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@UserID", journey.UserID);
-                    command.Parameters.AddWithValue("@TotalDistance", journey.TotalDistance);
-                    command.Parameters.AddWithValue("@TotalEmission", journey.TotalEmission);
-                    command.Parameters.AddWithValue("@Start", journey.Start);
-                    command.Parameters.AddWithValue("@End", journey.End);
-                    command.Parameters.AddWithValue("@Date", journey.Date);
-
-                    // Execute the query and get the newly inserted Journey ID
-                    int journeyID = 1;
-
-                    // Set the JourneyID property of the journey object to the newly inserted ID
-                    journey.JourneyID = journeyID;
-                }
-            }
-
-            // Redirect to the Details action to show the newly created journey
-            return RedirectToAction(nameof(Details), new { id = journey.JourneyID });
-        }
-
-        public IActionResult Edit(int id)
-        {
-            // Fetch a specific journey from the database
-            Journey journey = null;
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                string sql = "SELECT * FROM Journeys WHERE Journey_ID = @JourneyID";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@JourneyID", id);
-
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.Read())
-                    {
                         journey = new Journey
                         {
-                            JourneyID = (int)reader["Journey_ID"],
-                            UserID = (int)reader["User_ID"],
-                            TotalDistance = (int)reader["Total_Distance"],
-                            TotalEmission = (int)reader["Total_Emission"],
-                            Start = (string)reader["Start"],
-                            End = (string)reader["End"],
-                            Date = ((DateTime)reader["Date"]).Date
+                            Journey_ID = reader.GetInt32(0),
+                            User_ID = reader.GetInt32(1),
+                            Total_Distance = reader.GetInt32(2),
+                            Total_Emission = reader.GetInt32(3),
+                            Start = reader.GetString(4),
+                            End = reader.GetString(5),
+                            Date = reader.GetDateTime(6)
                         };
                     }
-
-                    reader.Close();
                 }
             }
 
-            // If the journey is not found, return a 404 error
-            if (journey == null)
-            {
-                return NotFound();
-            }
-
-            // Pass the journey to the view
             return View(journey);
         }
 
         [HttpPost]
-        public IActionResult Edit(int id, Journey journey)
+        public async Task<IActionResult> Edit(Journey journey)
         {
-            // Update an existing journey in the database
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                string sql = "UPDATE Journeys SET User_ID = @UserID, Total_Distance = @TotalDistance, Total_Emission = @TotalEmission, Start = @Start, [End] = @End, Date = @Date WHERE Journey_ID = @JourneyID";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+                using (SqlCommand cmd = new SqlCommand("UPDATE Journeys SET User_ID = @User_ID, Total_Distance = @Total_Distance, Total_Emission = @Total_Emission, Start = @Start, End = @End, Date = @Date WHERE Journey_ID = @Journey_ID", conn))
                 {
-                    command.Parameters.AddWithValue("@JourneyID", id);
-                    command.Parameters.AddWithValue("@UserID", journey.UserID);
-                    command.Parameters.AddWithValue("@TotalDistance", journey.TotalDistance);
-                    command.Parameters.AddWithValue("@TotalEmission", journey.TotalEmission);
-                    command.Parameters.AddWithValue("@Start", journey.Start);
-                    command.Parameters.AddWithValue("@End", journey.End);
-                    command.Parameters.AddWithValue("@Date", journey.Date);
+                    cmd.Parameters.AddWithValue("@Journey_ID", journey.Journey_ID);
+                    cmd.Parameters.AddWithValue("@User_ID", journey.User_ID);
+                    cmd.Parameters.AddWithValue("@Total_Distance", journey.Total_Distance);
+                    cmd.Parameters.AddWithValue("@Total_Emission", journey.Total_Emission);
+                    cmd.Parameters.AddWithValue("@Start", journey.Start);
+                    cmd.Parameters.AddWithValue("@End", journey.End);
+                    cmd.Parameters.AddWithValue("@Date", journey.Date);
 
-                    command.ExecuteNonQuery();
+                    conn.Open();
+                    await cmd.ExecuteNonQueryAsync();
                 }
             }
 
-            // Redirect to the Details action to show the updated journey
-            return RedirectToAction(nameof(Details), new { id });
+            return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int id)
+        // Delete
+        public async Task<IActionResult> Delete(int id)
         {
-            // Fetch a specific journey from the database
-            Journey journey = null;
+            Journey journey;
 
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
-                connection.Open();
-
-                string sql = "SELECT * FROM Journeys WHERE Journey_ID = @JourneyID";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Journeys WHERE Journey_ID = @Journey_ID", conn))
                 {
-                    command.Parameters.AddWithValue("@JourneyID", id);
+                    cmd.Parameters.AddWithValue("@Journey_ID", id);
 
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.Read())
+                    conn.Open();
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
                     {
+                        if (!await reader.ReadAsync())
+                        {
+                            return NotFound();
+                        }
+
                         journey = new Journey
                         {
-                            JourneyID = (int)reader["Journey_ID"],
-                            UserID = (int)reader["User_ID"],
-                            TotalDistance = (int)reader["Total_Distance"],
-                            TotalEmission = (int)reader["Total_Emission"],
-                            Start = (string)reader["Start"],
-                            End = (string)reader["End"],
-                            Date = ((DateTime)reader["Date"]).Date
+                            Journey_ID = reader.GetInt32(0),
+                            User_ID = reader.GetInt32(1),
+                            Total_Distance = reader.GetInt32(2),
+                            Total_Emission = reader.GetInt32(3),
+                            Start = reader.GetString(4),
+                            End = reader.GetString(5),
+                            Date = reader.GetDateTime(6)
                         };
                     }
-
-                    reader.Close();
                 }
-                return View(journey);
             }
 
-            // If the journey is not found, return a 404 error
-            if (journey == null)
-            {
-                return NotFound();
-            }
-
-            // Pass the journey to the view
             return View(journey);
         }
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            // Delete an existing journey from the database
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlConnection conn = new SqlConnection(_connectionString))
             {
-                connection.Open();
-
-                string sql = "DELETE FROM Journeys WHERE Journey_ID = @JourneyID";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Journeys WHERE Journey_ID = @Journey_ID", conn))
                 {
-                    command.Parameters.AddWithValue("@JourneyID", id);
+                    cmd.Parameters.AddWithValue("@Journey_ID", id);
 
-                    command.ExecuteNonQuery();
+                    conn.Open();
+                    await cmd.ExecuteNonQueryAsync();
                 }
             }
 
-            // Redirect to the Index action to show the updated list of journeys
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
     }
+
 }
--- CGI/Models/Journey.cs
+++ CGI/Models/Journey.cs
@@ -1,14 +1,31 @@
-﻿namespace CGI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CGI.Models
 {
     public class Journey
     {
-        public int JourneyID { get; set; }
-        public int UserID { get; set; }
-        public int TotalDistance { get; set; }
-        public int TotalEmission { get; set; }
-        public string? Start { get; set; }
-        public string? End { get; set; }
+        [Key]
+        public int Journey_ID { get; set; }
+
+        [Required]
+        public int User_ID { get; set; }
+
+        [Required]
+        public int Total_Distance { get; set; }
+
+        [Required]
+        public int Total_Emission { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Start { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string End { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
-        public List<Stopover> Stopovers { get; set; }
     }
 }
