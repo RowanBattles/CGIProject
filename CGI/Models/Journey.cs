9618bbe3e3047d744135dc25a5de77de756cae58
RowanBattles <rowanvanderweel@gmail.com>
2023-04-03 13:26:53 +0200

    CalculatePoints, models aangepast en database geupdate

--- CGI/Models/Journey.cs
+++ CGI/Models/Journey.cs
@@ -27,5 +27,10 @@ namespace CGI.Models
         [Required]
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
+        public int Score { get; set; }
+
+        // Total_Distance = List<Stopover> AllDistances
+        // Total_Emission = List<Stopover> AllEmission
     }
 }
