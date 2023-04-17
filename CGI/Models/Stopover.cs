6ef8bc91a39901b8bf02c681b83bf1274b1f36d0
Ku Tadao <kubilay12344@hotmail.nl>
2023-04-17 21:05:31 +0200

    Fix Enum values

--- CGI/Models/Stopover.cs
+++ CGI/Models/Stopover.cs
@@ -3,26 +3,28 @@
     public enum Vehicle
     {
         Walking = 1,
-        Bicycle,
-        ETrain, 
-        EBicycle,
-        EScooter,
-        ECar,
-        GScooter,
-        DTrain,
-        Tram,
-        Bus,
-        Metro,
-        HCar,
-        GMotorcycle,
-        DCar,
-        GCar,
+        Bicycle = 1,
+        ETrain = 2,
+        EBicycle = 3,
+        EScooter = 17,
+        ECar = 54,
+        GScooter = 62,
+        DTrain = 90,
+        Tram = 96,
+        Bus = 96,
+        Metro = 96,
+        HCar = 110,
+        GMotorcycle = 129,
+        DCar = 131,
+        GCar = 149,
 
         // E = Electric
         // G = Gasoline
         // H = Hybrid
         // D = Diesel
     }
+
+
 
     public class Stopover
     {
