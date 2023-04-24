41e03e5f62cc98b222daa12e0e82d052c15de54f
bilnik67 <hoflandbilly@gmail.com>
2023-04-24 23:14:31 +0200

    Edit Journey

--- CGI/Models/JourneyViewModel.cs
+++ CGI/Models/JourneyViewModel.cs
@@ -3,6 +3,8 @@
     public class JourneyViewModel
     {
         public int Id { get; set; }
+
+        public Journey Journey { get; set; }
         public List<Journey> Journeys { get; set; }
         public List<Stopover> Stopovers { get; set; }
     }
