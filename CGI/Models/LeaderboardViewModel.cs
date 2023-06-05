6724b46a521807c28559feacf7f6956fbfab07ea
Kat,Jonathan J.Z <j.kat@student.fontys.nl>
2023-06-05 12:40:35 +0000

    Update LeaderboardViewModel.cs

--- CGI/Models/LeaderboardViewModel.cs
+++ CGI/Models/LeaderboardViewModel.cs
@@ -1,15 +1,12 @@
-﻿namespace CGI.Models
+namespace CGI.Models
 {
     public class LeaderboardViewModel
     {
         public int Id { get; set; }
         public string userName { get; set; }
         public int score { get; set; }
-<<<<<<< Updated upstream
         public bool UserIsGroupAdmin { get; set; }
-=======
         public int lowerbound { get; set; }
         public int upperbound { get; set; }
->>>>>>> Stashed changes
     }
 }
